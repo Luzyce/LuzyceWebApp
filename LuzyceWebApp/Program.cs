30c10fa7855dd50f9ea93a73ab09af104d83fb46
Daniel <daniel.dominik.nowicki@gmail.com>
2024-06-27 14:29:00 +0200

    change items to positions

--- LuzyceWebApp/Program.cs
+++ LuzyceWebApp/Program.cs
@@ -9,7 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://phsapp.getin.ovh/") });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://phsapp.getin.ovh/") });
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
