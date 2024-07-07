cf0c775e90035199c3d3747018ca4823c69d0f77
Daniel <daniel.dominik.nowicki@gmail.com>
2024-07-07 07:12:48 +0200

    add printing and repair get QuantitiesInStock

--- LuzyceWebApp/Program.cs
+++ LuzyceWebApp/Program.cs
@@ -1,3 +1,4 @@
+using Append.Blazor.Printing;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using LuzyceWebApp;
@@ -9,9 +10,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://phsapp.getin.ovh/") });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("http://localhost:5132") });
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<IPrintingService, PrintingService>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<AccountService>();
