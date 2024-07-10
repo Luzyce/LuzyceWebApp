02597a61609349562ea279a367354279175010b9
Daniel <daniel.dominik.nowicki@gmail.com>
2024-07-10 21:24:38 +0200

    add SaveProductionPriority

--- LuzyceWebApp/Services/ProdPrioritiesService.cs
+++ LuzyceWebApp/Services/ProdPrioritiesService.cs
@@ -1,5 +1,7 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net;
+using System.Net.Http.Json;
 using Luzyce.Core.Models.ProductionOrder;
+using Luzyce.Core.Models.ProductionPriority;
 
 namespace LuzyceWebApp.Services;
 
@@ -12,5 +14,16 @@ public class ProdPrioritiesService(HttpClient httpClient, TokenValidationService
             return null;
         }
         return await httpClient.GetFromJsonAsync<GetOrdersPositionsResponse>($"/api/productionOrder/positions");
+        
+    }
+    
+    public async Task<bool> SaveProductionPriority(CreateProductionPriorityRequest request)
+    {
+        if (!await tokenValidationService.IsTokenValid())
+        {
+            return false;
+        }
+        var response = await httpClient.PostAsJsonAsync("/api/productionPriority/save", request);
+        return response.IsSuccessStatusCode && response.StatusCode != HttpStatusCode.Unauthorized && response.StatusCode != HttpStatusCode.Conflict;
     }
 }
